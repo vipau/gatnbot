4c7cdaa96bb1b6517ebea02bd9b3e54b2173986b
Vi Pau <696864+vipau@users.noreply.github.com>
2023-11-09 11:59:28 +0100

    switch to HTML; teach GPT4 how to output it properly

--- commands/commands.go
+++ commands/commands.go
@@ -215,8 +215,10 @@ func HandleCommands(configmap settings.Settings) *tb.Bot {
 					resp, err := client.ChatCompletion(context.Background(), gpt3.ChatCompletionRequest{
 						Messages: []gpt3.ChatCompletionRequestMessage{
 							{
-								Role:    "system",
-								Content: "You are GattiniBot, a bot in a group of people called Gattini. Be the most helpful but concise.",
+								Role: "system",
+								Content: "You are GattiniBot, a bot in a group of people called Gattini. Be the most helpful but concise." +
+									" Output simple HTML. You can only make use of the HTML tags a, b, i, s, u, code (for monospace text)." +
+									" If you need a code block, use 'pre'. Do not use any other tag, including br.",
 							},
 							{
 								Role:    "user",
@@ -238,7 +240,7 @@ func HandleCommands(configmap settings.Settings) *tb.Bot {
 							_, err = b.Reply(c.Message(), "gatnbot warning: response is empty!")
 							checkPrintErr(err)
 						} else {
-							opts := &tb.SendOptions{DisableWebPagePreview: true, ParseMode: "Markdown"}
+							opts := &tb.SendOptions{DisableWebPagePreview: true, ParseMode: "HTML"}
 							_, err = b.Reply(c.Message(), resp.Choices[0].Message.Content, opts)
 							checkPrintErr(err)
 						}
